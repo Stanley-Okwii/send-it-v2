3a90ca7ad19710b944edc9f481c2ba8acb6b59eb
Stanley-Okwii <stanleeparker12@gmail.com>
2019-01-25 13:13:05 +0300

    Add conditional visibility for log in tab

--- src/components/SendItApp.js
+++ src/components/SendItApp.js
@@ -5,6 +5,14 @@ import 'semantic-ui-css/semantic.min.css';
 import '../css/App.css';
 
 class SendItApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLogInTab: true
+    };
+  }
+
   render() {
     return (
       <div>
@@ -14,29 +22,73 @@ class SendItApp extends Component {
               SendIT Courier Services
                 <Image src='logo.png' />
             </Header>
-            <Form className='form-wrapper' size='large'>
-              <Button.Group>
-                <Button icon='sign-in' content='Sign In' />
-                <Button icon='user plus' content='Sign Up' />
-              </Button.Group>
-              <Segment vertical>
-                <Form.Field>
-                  <label className='label-align'>Email</label>
-                  <input placeholder='Email' />
-                </Form.Field>
-                <Form.Field>
-                  <label className='label-align' >Password</label>
-                  <input placeholder='Password' />
-                </Form.Field>
-                <Button className='index-button' color='teal'>
-                  Login
+            {this.state.isLogInTab ? (
+              <Form className='form-wrapper' size='large'>
+                <Button.Group>
+                  <Button icon='sign-in' onClick={this.showLogin} content='Sign In' />
+                  <Button icon='user plus' onClick={this.showSignUp} content='Sign Up' />
+                </Button.Group>
+                <Segment vertical>
+                  <Form.Field>
+                    <label className='label-align'>Email</label>
+                    <input placeholder='Email' />
+                  </Form.Field>
+                  <Form.Field>
+                    <label className='label-align' >Password</label>
+                    <input type='password' placeholder='Password' />
+                  </Form.Field>
+                  <Button className='index-button' color='teal'>
+                    Login
             </Button>
-              </Segment>
-            </Form>
+                </Segment>
+              </Form>
+            )
+              :
+              (
+                <Form className='form-wrapper' size='large'>
+                  <Button.Group>
+                    <Button icon='sign-in' onClick={this.showLogin} content='Sign In' />
+                    <Button icon='user plus' onClick={this.showSignUp} content='Sign Up' />
+                  </Button.Group>
+                  <Segment vertical>
+                    <Form.Field>
+                      <label className='label-align'>User Name</label>
+                      <input placeholder='User Name' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label className='label-align' >Email</label>
+                      <input type='email' placeholder='Email' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label className='label-align' >Password</label>
+                      <input type='password' placeholder='Password' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label className='label-align' >Confirm password</label>
+                      <input type='password' placeholder='Confirm password' />
+                    </Form.Field>
+                    <Button className='index-button' color='teal'>
+                      sign up
+          </Button>
+                  </Segment>
+                </Form>
+              )
+            }
           </Grid.Column>
         </Grid>
       </div>
     );
+  }
+
+  showLogin = () => {
+    this.setState({ isLogInTab: true });
+    console.log('showing log in');
+  }
+
+  showSignUp = () => {
+    this.setState({ isLogInTab: false });
+    console.log('showing sign up');
+
   }
 }
 
