d3764d2d4140aa94d4fcfc9374c4a76260c9cc10
Stanley-Okwii <stanleeparker12@gmail.com>
2019-01-25 12:27:03 +0300

    style login tab

--- src/components/SendItApp.js
+++ src/components/SendItApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Grid, Header , Button, Segment, Message, Image} from 'semantic-ui-react';
+import { Form, Grid, Header, Button, Segment, Image } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 import '../css/App.css';
@@ -8,33 +8,34 @@ class SendItApp extends Component {
   render() {
     return (
       <div>
-      <Grid textAlign='center' style={{ height: '60%' }} verticalAlign='middle'>
+        <Grid textAlign='center' style={{ height: '60%' }} verticalAlign='middle'>
           <Grid.Column style={{ maxWidth: 600 }}>
-              <Header as='h2' color='teal' textAlign='center'>
-                SendIT Courier Services
-                <Image src='logo.png' /> 
-              </Header>
-              <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
-            <Button color='teal' fluid size='large'>
-              Login
+            <Header as='h2' color='teal' textAlign='center'>
+              SendIT Courier Services
+                <Image src='logo.png' />
+            </Header>
+            <Form className='form-wrapper' size='large'>
+              <Button.Group>
+                <Button icon='sign-in' content='Sign In' />
+                <Button icon='user plus' content='Sign Up' />
+              </Button.Group>
+              <Segment vertical>
+                <Form.Field>
+                  <label className='label-align'>Email</label>
+                  <input placeholder='Email' />
+                </Form.Field>
+                <Form.Field>
+                  <label className='label-align' >Password</label>
+                  <input placeholder='Password' />
+                </Form.Field>
+                <Button className='index-button' color='teal'>
+                  Login
             </Button>
-          </Segment>
-        </Form>
-        <Message>
-          New to us? <a href='d'>Sign Up</a>
-        </Message>
-        </Grid.Column>
-      </Grid>
-  </div>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
