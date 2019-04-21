124ea0b1e78a4c96fc248c276c179dbfe0d0986b
Stanley-Okwii <stanleeparker12@gmail.com>
2019-04-21 22:57:30 +0300

    Set up an enviroment for testing [Delivers #165485905]

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { shallow } from "enzyme";
+import SendItApp from '../components/SendItApp';
 
-// it('renders without crashing', () => {
-// });
+it('renders without crashing', () => {
+    const wrapper = shallow(<SendItApp />);
+    expect(wrapper).toMatchSnapshot();
+});
