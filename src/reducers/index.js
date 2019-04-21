228cb61912778a1fda322a9b772ba0327dd0b098
Stanley-Okwii <stanleeparker12@gmail.com>
2019-04-21 21:32:14 +0300

    [Update] code clean up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
 import isLogInTab from './isLoginTab'
 
 export default combineReducers({
-  todos,
-  visibilityFilter,
   isLogInTab
 })
--- src/routes.js
+++ src/routes.js
@@ -2,29 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createBrowserHistory } from 'history';
 import SendItApp from "./components/SendItApp";
-import { App } from "./components/App";
 
 
 const DefaultApp = () => (
   <Router history={createBrowserHistory}>
     <div>
       <Route exact path="/" component={SendItApp} />
-      <Route path="/app" component={App} />
     </div>
   </Router>
 );
-
-// function Header() {
-//   return (
-//     <ul>
-//       <li>
-//         <Link to="/">Todo</Link>
-//       </li>
-//       <li>
-//         <Link to="/sendit">Send it</Link>
-//       </li>
-//     </ul>
-//   );
-// }
 
 export default DefaultApp;
